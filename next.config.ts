afcfb382c76ca6e309a819f5ad6e1fdb8738ff19
rohit singh <rohitsingh95083318@gmail.com>
2025-08-15 18:46:25 +0530

    changes made

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ webpack: (config) => {
+    config.externals = [...config.externals, '@prisma/client'];
+    return config;
+  }
 };
 
 export default nextConfig;
